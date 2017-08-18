455c207c168afdb59ef9c52be3f59c1712ef5f4e
Арепьев Иван Юриевич <ivan.arepjev@gmail.com>
2017-08-19 00:42:06 +0300

    первые попытки

--- TCP_Chat_Server/MainWindow.xaml.cs
+++ TCP_Chat_Server/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,53 @@ namespace TCP_Chat_Server
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int port = 8888;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            TcpListener server = null;
+
+            try
+            {
+                server = new TcpListener(IPAddress.Parse("192.168.0.102"), port);
+                server.Start();
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    await  Task.Factory.StartNew( () => Listener(client),TaskCreationOptions.LongRunning);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
+        }
+
+        void Listener(Object o)
+        {
+            TcpClient client = (TcpClient)o;
+            NetworkStream stream = client.GetStream();
+            StreamReader sw = new StreamReader(stream);
+            string answer = sw.ReadToEnd();
+            lock (this)
+            {
+                Logins.Text += answer + "\n";
+            }
+            
+
+            sw.Close();
+            client.Close();
+            
         }
     }
 }
