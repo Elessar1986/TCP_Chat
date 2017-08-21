e4f2d5cf8eade0ec7973f744105f21e9087b23c7
Арепьев Иван Юриевич <ivan.arepjev@gmail.com>
2017-08-21 18:11:01 +0300

    сделал рабочее клиентское приложение на wpf

---
+++ TCPChat/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCPChat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Source source;
+        static string userName;
+        private const string host = "192.168.0.102";
+        private const int port = 20000;
+        static TcpClient client;
+        static NetworkStream stream;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            source = new Source();
+            this.DataContext = source;
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+                userName = Guid.NewGuid().ToString();
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();  
+        }
+
+        private void SendMessage()
+        {
+            string message = source.message;
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            source.message = "";
+        }
+
+        private void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    source.mainText += message + Environment.NewLine;
+                }
+                catch
+                {
+                    MessageBox.Show("Подключение прервано!"); //соединение было прервано
+                    
+                    Disconnect();
+                }
+            }
+        }
+
+        private void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();//отключение потока
+            if (client != null)
+                client.Close();//отключение клиента
+            Environment.Exit(0); //завершение процесса
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+                userName = Guid.NewGuid().ToString();
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+
+        }
+    }
+}
