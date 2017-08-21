df935374f76a3eb8f9926a9159c46aa7a09e0805
Арепьев Иван Юриевич <ivan.arepjev@gmail.com>
2017-08-21 14:56:47 +0300

    Отказался от бредовой идеи сервака на wpf. Приступил к созданию его в консольке

--- TCPServer/Program.cs
+++ TCPServer/Program.cs
@@ -8,69 +8,20 @@ using System.Threading.Tasks;
 
 namespace TCPServer
 {
-    class Client : IDisposable
-    {
-        NetworkStream s;
-
-        public Client(TcpClient client)
-        {
-            s = client.GetStream();
-        }
-
-        public void Dispose()
-        {
-            s.Dispose();
-        }
-
-        async Task<string> ReadFromStreamAsync()
-        {
-            var buf = new byte[s.Length];
-            var readpos = 0;
-            //while (readpos < nbytes)
-            //    readpos += await s.ReadAsync(buf, readpos, nbytes - readpos);
-            await s.ReadAsync(buf, 0, Convert.ToInt32(s.Length));
-            string str = Encoding.UTF8.GetString(buf);
-            return str;
-        }
-
-        public async Task ProcessAsync()
-        {
-            while (true)
-            {
-                string res = await ReadFromStreamAsync();
-                Console.WriteLine(res + "\n");
-            }
-        }
-    }
-
+   
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("starting server...");
-            RunServerAsync();
+
+            new Server(20000);
 
             Console.ReadKey();
         }
 
-        public static async void RunServerAsync()
-        {
-
-            var tcpListener = new TcpListener(IPAddress.Any,20000);
-            tcpListener.Start();
-            Console.WriteLine("server start " + DateTime.Now.ToLongDateString());
-            while (true) // тут какое-то разумное условие выхода
-            {
-                var tcpClient = await tcpListener.AcceptTcpClientAsync();
-                processClientTearOff(tcpClient); // await не нужен
-            }
-        }
-
-        async static Task processClientTearOff(TcpClient c)
-        {
-            using (var client = new Client(c))
-                await client.ProcessAsync();
-        }
+       
     }
 }
 
---
+++ TCPServer/Server.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Server
+    {
+
+        TcpListener Listener; // Объект, принимающий TCP-клиентов
+
+        // Запуск сервера
+        public  Server(int Port)
+        {
+            Console.WriteLine("Starting server...");
+            // Создаем "слушателя" для указанного порта
+            Listener = new TcpListener(IPAddress.Any, Port);
+            Listener.Start(); // Запускаем его
+            Console.WriteLine($"Server start {DateTime.Now.ToLongTimeString()}");
+            Console.WriteLine("Waiting for clients...");
+            // В бесконечном цикле
+            while (true)
+            {
+                // Принимаем нового клиента
+                TcpClient Client = Listener.AcceptTcpClient();
+                // Создаем поток
+                Thread thread = new Thread(new ParameterizedThreadStart(ClientThread));
+                // И запускаем этот поток, передавая ему принятого клиента
+                thread.Start(Client);
+
+                //ListenerRun();
+            }
+        }
+
+        // Остановка сервера
+        ~Server()
+        {
+            // Если "слушатель" был создан
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+
+        public void Close()
+        {
+            Listener.Stop();
+        }
+
+        private async void ListenerRun()
+        {
+            // Принимаем нового клиента
+            TcpClient Client = await Listener.AcceptTcpClientAsync();
+            // Создаем поток
+            Thread thread = new Thread(new ParameterizedThreadStart(ClientThread));
+            // И запускаем этот поток, передавая ему принятого клиента
+            thread.Start(Client);
+        }
+
+
+        static void ClientThread(Object StateInfo)
+        {
+            new Client((TcpClient)StateInfo);
+        }
+    }
+
+
+}
